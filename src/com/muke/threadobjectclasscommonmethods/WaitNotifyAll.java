ad91bf4331004bd86b2cca5b64691ac7aad4a869
zilongw <zilongwang1998@163.com>
2023-03-27 16:08:48 +0800

    "WaitNotifyReleaseOwnMonitor"

--- src/com/muke/threadobjectclasscommonmethods/WaitNotifyAll.java
+++ src/com/muke/threadobjectclasscommonmethods/WaitNotifyAll.java
@@ -4,6 +4,25 @@ public class WaitNotifyAll implements Runnable {
 
     private static final Object resourceA = new Object();
 
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = new WaitNotifyAll();
+        Thread threadA = new Thread(r);
+        Thread threadB = new Thread(r);
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (resourceA) {
+                    resourceA.notifyAll();
+                    System.out.println("ThreadC notified.");
+                }
+            }
+        });
+        threadA.start();
+        threadB.start();
+        Thread.sleep(200);
+        threadC.start();
+    }
+
     @Override
     public void run() {
         synchronized (resourceA) {
@@ -11,6 +30,7 @@ public class WaitNotifyAll implements Runnable {
             try {
                 System.out.println(Thread.currentThread().getName() + "wait to start.");
                 resourceA.wait();
+                System.out.println(Thread.currentThread().getName() + "s waiting to end.");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
---
+++ src/com/muke/threadobjectclasscommonmethods/WaitNotifyReleaseOwnMonitor.java
@@ -0,0 +1,4 @@
+package com.muke.threadobjectclasscommonmethods;
+
+public class WaitNotifyReleaseOwnMonitor {
+}
